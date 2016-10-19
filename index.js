85b12643a157ec342627198ecff5b9c96773fcb2
Garrett Murphey <gmurphey@trovedata.com>
2016-10-19 19:45:13 -0400

    Fixing in nested addon scenario.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ module.exports = {
   name: 'ember-version-is',
 
   included: function(app) {
+    while (typeof app.import !== 'function' && (app.app || app.parent)) {
+      app = app.app || app.parent;
+    }
+
     this._super.included(app);
     app.import('vendor/semver.browser.js');
   }
